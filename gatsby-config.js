2bbbcb01ba1cc027c3fc4a34f7093a74bb6403da
Andrew Bryson <kozmcode@gmail.com>
2018-08-02 18:45:33 +0200

    init

---
+++ gatsby-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Lion Tongue',
+  },
+  plugins: ['gatsby-plugin-react-helmet', 'gatsby-plugin-typescript', 'gatsby-plugin-postcss'],
+}
---
+++ src/layouts/index.tsx
@@ -0,0 +1,40 @@
+import * as React from 'react'
+import Helmet from 'react-helmet'
+
+import Index from '../components/index'
+import './index.css'
+
+const Layout = ({ children, data }) => (
+  <div>
+    <Helmet
+      title={data.site.siteMetadata.title}
+      meta={[
+        { name: 'description', content: 'Sample' },
+        { name: 'keywords', content: 'sample, something' },
+      ]}
+    />
+    <Index siteTitle={data.site.siteMetadata.title} />
+    <div
+      style={{
+        margin: '0 auto',
+        maxWidth: 960,
+        padding: '0px 1.0875rem 1.45rem',
+        paddingTop: 0,
+      }}
+    >
+      {children()}
+    </div>
+  </div>
+)
+
+export default Layout
+
+export const query = graphql`
+  query SiteTitleQuery {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
